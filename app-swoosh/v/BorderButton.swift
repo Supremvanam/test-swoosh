ccba27f8b78393d1bbdc26750e1510229649ab8f
Suprem Vanam <supremvanam.gsp@gmail.com>
2017-08-28 15:57:44 +0530

    Added BorderButton Class

---
+++ app-swoosh/v/BorderButton.swift
@@ -0,0 +1,21 @@
+//
+//  BorderButton.swift
+//  app-swoosh
+//
+//  Created by Suprem Vanam on 28/08/17.
+//  Copyright © 2017 Suprem Vanam. All rights reserved.
+//
+
+import UIKit
+
+class BorderButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
