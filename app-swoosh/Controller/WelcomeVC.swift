6dcf0f296743ea6194b05a336018b2b8f045e3b9
Suprem Vanam <supremvanam.gsp@gmail.com>
2017-08-29 21:51:32 +0530

    Refactored Classes

---
+++ app-swoosh/Controller/WelcomeVC.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  app-swoosh
+//
+//  Created by Suprem Vanam on 28/08/17.
+//  Copyright © 2017 Suprem Vanam. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeVC: UIViewController {
+
+    @IBOutlet weak var swoosh: UIImageView!
+    @IBOutlet weak var bgImg: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        swoosh.frame = CGRect(x: view.frame.size.width / 2 - swoosh.frame.size.width / 2, y: 50, width: swoosh.frame.size.width, height: swoosh.frame.size.height)
+//        
+//        bgImg.frame = view.frame
+    }
+    
+    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue) {
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
